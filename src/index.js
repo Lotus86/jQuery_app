9e99e5a9a12804fe9059afbf786fe591197c81e1
Gennady <gborodin86@gmail.com>
2019-06-02 22:44:25 +0300

    4-add-feature-to-delete-list-items

--- src/index.js
+++ src/index.js
@@ -1,18 +1,28 @@
+var $todoInput       = $('.todo-input');
+var $todoList        = $('.todo-list');
+var $todoBtn         = $('.todo-btn');
+var $todoItem        = $('.todo-item');
+var $todoItemDelete  = $('.todo-item-delete');
+
 function addToList() {
-    var newItem = $('.todo-input').val().trim();
-    $('.todo-list').append('<li class="todo-item">' + newItem + '</li>');
-    $('.todo-input').val('').trigger('input');
+    var newItem = $todoInput.val().trim();
+    $todoList.append('<li class="todo-item">' + newItem + '<button class="todo-item-delete"><i class="fas fa-trash-alt"></i></button>' + '</li>');
+    $todoInput.val('').trigger('input');
 };
 
-$ ('.todo-btn').on('click', addToList);
+$todoBtn.on('click', addToList);
 
 $(document).ready(function(){
-    $('.todo-btn').attr('disabled',true);
-    $('.todo-input').on('input', function() {
+    $todoBtn.attr('disabled',true);
+    $todoInput.on('input', function() {
         if ($(this).val().trim().length !== 0) {
-            $('.todo-btn').attr('disabled', false);
+            $todoBtn.attr('disabled', false);
         } else {
-            $('.todo-btn').attr('disabled', true);
+            $todoBtn.attr('disabled', true);
         }
     })
 });
+
+$('.todo-list').on('click', '.todo-item-delete', function() {
+    $(this).parent().remove();
+} );
