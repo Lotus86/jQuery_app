ee4badf83f7dbe8074e009f0d602c8ff5ea14ba3
Gennady <gborodin86@gmail.com>
2019-06-04 00:23:29 +0300

    modified a list to checkbox type

--- src/index.js
+++ src/index.js
@@ -6,23 +6,31 @@ var $todoItemDelete  = $('.todo-item-delete');
 
 function addToList() {
     var newItem = $todoInput.val().trim();
-    $todoList.append('<li class="todo-item">' + newItem + '<button class="todo-item-delete"><i class="fas fa-trash-alt"></i></button>' + '</li>');
+    $todoList.append(
+        '<li class="todo-list">' +
+        '<input class="todo-item" type="checkbox">' +
+        '<span class="item-text">' + newItem + '</span>' +
+        '<button class="todo-item-delete"><i class="fas fa-trash-alt"></i></button>' + '</input>' +
+        '</li>'
+    );
     $todoInput.val('').trigger('input');
 };
 
 $todoBtn.on('click', addToList);
 
+//Disable the Add button
 $(document).ready(function(){
-    $todoBtn.attr('disabled',true);
+    $todoBtn.prop('disabled',true);
     $todoInput.on('input', function() {
         if ($(this).val().trim().length !== 0) {
-            $todoBtn.attr('disabled', false);
+            $todoBtn.prop('disabled', false);
         } else {
-            $todoBtn.attr('disabled', true);
+            $todoBtn.prop('disabled', true);
         }
     })
 });
 
+//Remove items from the Todolist
 $('.todo-list').on('click', '.todo-item-delete', function() {
     $(this).parent().remove();
-} );
+});
