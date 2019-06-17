c0a27f08a799c373b40923ac338c1be9ac7720bb
Gennady <gborodin86@gmail.com>
2019-06-17 23:13:00 +0300

    checked/unchecked feature was added

--- src/index.js
+++ src/index.js
@@ -1,22 +1,34 @@
 var
     $todoInput       = $('.todo-input'),
     $todoList        = $('.todo-list'),
-    $todoBtn         = $('.todo-btn'),
-    $todoItem        = $('.todo-item'),
-    $todoItemDelete  = $('.todo-item-delete');
+    $todoBtn         = $('.todo-btn');
 
+//Add items list
 function addToList() {
-    var newItem = $todoInput.val().trim();
-    $todoList.append(
-        '<li class="todo-list">' +
-        '<input class="todo-item" type="checkbox">' +
-        '<span class="item-text">' + newItem + '</span>' +
-        '<button class="todo-item-delete"><i class="fas fa-trash-alt"></i></button>' + '</input>' +
-        '</li>'
-    );
-    $todoInput.val('').trigger('input');
-};
+    var $newItem = $todoInput.val().trim();
+    if(!$newItem)
+        return $todoInput.val('');
 
+    $newItem =  $('<li class="todo-list">' +
+        '<input class="todo-item" type="checkbox">' +
+        '<span class="item-text">' + $newItem + '</span>' +
+        '<button class="todo-item-delete"><i class="fas fa-times"></i></button>' + '</input>' +
+        '</li>');
+
+    $todoList.append($newItem);
+    $todoInput.val('').trigger('input');
+
+    //Checked|unchecked list items
+    $newItem.on('click', '.todo-item', function() {
+        if ($(this).prop('checked')) {
+            $('.item-text').addClass('item-text-decoration');
+        } else {
+            $('.item-text').removeClass('item-text-decoration');
+        }
+    });
+}
+
+//Add items by Enter button
 $todoInput.keypress(function(e){
     var keycode = (e.keyCode ? e.keyCode : e.which);
     if(keycode == '13'){
@@ -25,18 +37,6 @@ $todoInput.keypress(function(e){
 });
 
 $todoBtn.on('click', addToList);
-
-//Disable the Add button
-$(document).ready(function(){
-    $todoBtn.prop('disabled',true);
-    $todoInput.on('input', function() {
-        if ($(this).val().trim().length !== 0) {
-            $todoBtn.prop('disabled', false);
-        } else {
-            $todoBtn.prop('disabled', true);
-        }
-    })
-});
 
 //Remove items from the Todolist
 $('.todo-list').on('click', '.todo-item-delete', function() {
